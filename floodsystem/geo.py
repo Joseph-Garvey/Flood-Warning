0b3d7289c5d652dfcee10e4ae1ac4b051cb5bfb8
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-02-04 20:51:03 +0000

    geo without sort

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,22 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+
+
+def stations_by_distance(stations, p):
+    for i in range(len(stations)):
+        # Find distance of each station from coordinate p
+        distance = haversine(stations[i].coord, p)
+
+        # Create tuple with station & distance
+        distance_tuple = (stations[i], distance)
+
+        stations_and_distance = []
+
+        # Add tuple to list
+        stations_and_distance.append(distance_tuple)
+
+    # Sort list by distance
+
+    return stations_and_distance

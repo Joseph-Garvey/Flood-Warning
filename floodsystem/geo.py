9914d5ec7029e90f999141370c633c30715fd6bf
Joseph Garvey <josephgarvey784@gmail.com>
2022-02-04 20:08:52 +0000

    Establish branch and haversine distance shortcut.

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,18 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+
+
+def distance(coord, p): # increase readability
+    return haversine(coord, p)
+    
+### Task 1C
+# return list of all stations within rad r of a geo coord x
+# remember distance between two geo points is computed by haversine
+def stations_within_radius(stations, centre, r):
+    result = []
+    for i in range(len(stations)):
+        if(haversine(stations[i].coord, centre) < r): # it would never be equal to. these are floats.
+            result.append(stations[i])
+

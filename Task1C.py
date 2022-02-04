505239fb2e7eb739d10edb4c0be26249d8e81b78
Joseph Garvey <josephgarvey784@gmail.com>
2022-02-04 20:37:38 +0000

    Fixed docstring location.

--- Task1C.py
+++ Task1C.py
@@ -1,1 +1,22 @@
-# use geo.stations within radius
+# dependencies
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+# main
+def run():
+    """Requirements for Task 1C"""
+
+    # Build list of stations
+    stations = build_station_list()
+    # Filter list by stations within 10km of Cambridge City Centre
+    # City Centre has coords (52.2053, 0.1218)
+    stations_filtered = stations_within_radius(stations, (52.2053, 0.1218), 10)
+    # sort alphabetical
+    stations_filtered.sort()
+    # output the filtered list.
+    for station in stations_filtered:
+        print(station.name)
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: MIT
 """This module contains a collection of functions related to
 geographical data.
-
 """
 
 from .utils import sorted_by_key  # noqa
@@ -16,14 +15,14 @@ def distance(coord, p): # increase readability
 ### Task 1C
 # return list of all stations within rad r of a geo coord x
 # remember distance between two geo points is computed by haversine
-"""Returns a list of stations within radius r of a given co-ordinate.
-
+def stations_within_radius(stations, centre, r):
+    """Returns a list of stations within radius r of a given co-ordinate.
+    
     Params:
         Stations - List of Stations to be Filtered
         Centre - The coordinate around which the radius is measured. The 'search parameter'.
         R - The radius by which the function filters in km.
-"""
-def stations_within_radius(stations, centre, r):
+    """
     result = []
     for i in range(len(stations)):  
         if(haversine(stations[i].coord, centre) < r): # it would never be exactly equal to anyways.

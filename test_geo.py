bc59ba49b1fb76cd080ef6ec93375ccf4b53ae61
Joseph Garvey <josephgarvey784@gmail.com>
2022-02-05 21:07:10 +0000

    fixed geo.py

--- test_geo.py
+++ test_geo.py
@@ -3,5 +3,4 @@ from floodsystem.geo import *
 def test_distance():
     coord1 = [15.0185, 30.2026]
     coord2 = [23.5076, 1.101]
-    assert distance(coord1, coord2) >= 3190.12
-    assert distance(coord1, coord2) <= 3190.14
+    assert round(distance(coord1, coord2), 6) == 3190.13

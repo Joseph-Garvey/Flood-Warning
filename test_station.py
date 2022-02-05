43b5377ba025ad2b7de928c8c89d6c3095d43fc6
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-02-05 23:52:37 +0000

    Task 1F testing first complete draft (verify if can be more efficient using for loops(?))

--- test_station.py
+++ test_station.py
@@ -4,6 +4,7 @@
 """Unit test for the station module"""
 
 from floodsystem.station import MonitoringStation
+from floodsystem.station import inconsistent_typical_range_stations
 
 
 def test_create_monitoring_station():
@@ -25,3 +26,43 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+
+# region Task 1F Testing
+
+# Testing Task 1F part i
+def test_typical_range_consistent():
+    # Create 3 dummy stations, 1 with consistent data, 1 with empty data, 1 with inconsistent low/high data
+    station_A = MonitoringStation('ID A', 'Measurement ID A', 'Name A', (10.0, 40.0), None, 'Medium River', 'Town A')
+    station_B = MonitoringStation('ID B', 'Measurement ID B', 'Name B', (0.0, 10.0), (1.0, 2.0), 'Big River', 'Town B')
+    station_C = MonitoringStation('ID C', 'Measurement ID C', 'Name C', (0.0, 20.0), (2.0, 1.0), 'Big River', 'Town C')
+
+    assert station_A.typical_range_consistent() == False
+    assert station_B.typical_range_consistent() == True
+    assert station_C.typical_range_consistent() == False
+
+# Testing Task 1F part ii
+
+
+def test_inconsistent_typical_range_stations():
+    # Create 3 dummy stations, 1 with consistent data, 1 with empty data, 1 with inconsistent low/high data
+    station_A = MonitoringStation('ID A', 'Measurement ID A', 'Name A', (10.0, 40.0), None, 'Medium River', 'Town A')
+    station_B = MonitoringStation('ID B', 'Measurement ID B', 'Name B', (0.0, 10.0), (1.0, 2.0), 'Big River', 'Town B')
+    station_C = MonitoringStation('ID C', 'Measurement ID C', 'Name C', (0.0, 20.0), (2.0, 1.0), 'Big River', 'Town C')
+
+    station_list = [station_A, station_B, station_C]
+
+    # Assert that output is a list
+    assert type(inconsistent_typical_range_stations(station_list)) == list
+
+    # Assert that each entry has the correct type
+    assert type(inconsistent_typical_range_stations(station_list)[0]) == MonitoringStation
+
+    # Assert that list has length 2
+    assert len(inconsistent_typical_range_stations(station_list)) == 2
+
+    # Assert that all are False
+    assert inconsistent_typical_range_stations(station_list)[0].typical_range_consistent() == False
+    assert inconsistent_typical_range_stations(station_list)[1].typical_range_consistent() == False
+
+# endregion

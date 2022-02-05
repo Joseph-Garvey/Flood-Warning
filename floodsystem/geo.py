075ef0432e12e01cf18ef0721ef08d9bfad302ef
Joseph Garvey <josephgarvey784@gmail.com>
2022-02-05 16:03:58 +0000

    Merge 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -3,13 +3,34 @@
 # SPDX-License-Identifier: MIT
 """This module contains a collection of functions related to
 geographical data.
-
 """
 
 from .utils import sorted_by_key  # noqa
-from haversine import haversine, Unit
+from haversine import haversine
 
 
+def distance(coord, p): # increase readability
+    
+    return haversine(coord, p)
+    
+### Task 1C
+# return list of all stations within rad r of a geo coord x
+# remember distance between two geo points is computed by haversine
+def stations_within_radius(stations, centre, r):
+    """Returns a list of stations within radius r of a given co-ordinate.
+
+    Params:
+        Stations - List of Stations to be Filtered. \n
+        Centre - The coordinate around which the radius is measured. The 'search parameter'. \n
+        R - The radius by which the function filters in km.
+    """
+    result = []
+    for i in range(len(stations)):  
+        if(haversine(stations[i].coord, centre) < r): # it would never be exactly equal to anyways.
+            result.append(stations[i])
+    return result
+
+### Task 1B
 def stations_by_distance(stations, p):
     """Returns a list of tuples containing stations and their distances from coordinate P.
 

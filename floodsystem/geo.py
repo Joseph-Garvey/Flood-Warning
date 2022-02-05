8af461a27c946c793cdedd29c99e222a9c120147
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-02-05 15:40:32 +0000

    Task 1E First Step

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -29,3 +29,23 @@ def stations_by_distance(stations, p):
     sorted_by_key(stations_and_distance, 1)
 
     return stations_and_distance
+
+
+def rivers_by_station_number(stations, N):
+    """Returns a list of the first N (river name, number of stations) tuples with the greatest number of stations.
+
+    Params:
+        stations - list of stations to be filtered \n
+        N - number of tuples to show
+    """
+
+    # Create empty list
+    rivers_N_list = []
+
+    # Loop over each station
+    for i in range(len(rivers_with_station)):
+        # Create tuple of (river name, number of stations)
+        tuple =
+        # Add tuple to list
+
+        # Sort list by number of stations

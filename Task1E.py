7182b54ee6256161685d45789491a5ebb5c5246f
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-02-05 17:40:52 +0000

    Task-1E (rivers_by_station_number demo done, Task1B broken: coord attribute not found)

---
+++ Task1E.py
@@ -0,0 +1,11 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
+
+
+def run():
+    """Task 1E: prints the list of (river, number stations) tuples when N = 9"""
+
+
+if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,18 +9,20 @@ geographical data.
 from .utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
 
+# Task 1B
+
 
 def stations_by_distance(stations, p):
 
     # Create empty list
     stations_and_distance = []
 
-    for i in range(len(stations)):
+    for station in stations:
         # Find distance of each station from coordinate p
-        distance = haversine(stations[i].coord, p)
+        distance = haversine(stations.coord, p)
 
         # Create tuple with station & distance
-        distance_tuple = (stations[i], distance)
+        distance_tuple = (stations, distance)
 
         # Add tuple to list
         stations_and_distance.append(distance_tuple)
@@ -30,22 +32,43 @@ def stations_by_distance(stations, p):
 
     return stations_and_distance
 
+# Task 1E
+
 
 def rivers_by_station_number(stations, N):
     """Returns a list of the first N (river name, number of stations) tuples with the greatest number of stations.
 
     Params:
         stations - list of stations to be filtered \n
-        N - number of tuples to show
+        N - number of tuples to show \n
+        Pulls river name & list of stations by river from the dictionary stations_by_river
     """
 
     # Create empty list
-    rivers_N_list = []
+    rivers_n_list = []
 
     # Loop over each station
-    for i in range(len(rivers_with_station)):
-        # Create tuple of (river name, number of stations)
-        tuple =
-        # Add tuple to list
+    for i in stations_by_river:
 
-        # Sort list by number of stations
+        # Create tuple of (river name, number of stations)
+        river_n_tuple = (i, len(stations_by_river[i]))
+
+        # Add tuple to list
+        rivers_n_list.append(river_n_tuple)
+
+    # Sort list by number of stations
+    sorted_list = sorted_by_key(rivers_n_list, 1, True)
+
+    # Slice list for first N
+    first_N_rivers = sorted_list[:N]
+
+    # Loop over rest of list
+    for i in range(N, len(sorted_list)):
+
+        # check if number of stations is the same as number of stations in Nth river
+        if sorted_list[i][1] == first_N_rivers[N - 1][1]:
+
+            # append tuple to sorted list
+            first_N_rivers.append(sorted_list[i])
+
+    return first_N_rivers

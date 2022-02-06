560605dabd9fc48ecf7cf2fdc1d78a9ac61a0066
Joseph Garvey <josephgarvey784@gmail.com>
2022-02-06 18:25:19 +0000

    Task 1D deliverables now actually delivered.

--- Task1D.py
+++ Task1D.py
@@ -9,9 +9,28 @@ def run():
     # Build list of stations
     stations = build_station_list()
     # print how many rivers have at least one monitoring station
-    print(len(rivers_with_station(stations)))
+    rivers = rivers_with_station(stations)
+    print(len(rivers), "Stations.")
     # print first 10 in alphabetical order
-    print(stations_by_river(stations))
+    # sets cannot slice therefore cast to list
+    output = list(rivers)
+    output.sort()
+    print("First 10 -", output[:10])
+    # first ten printed in alphabetical order
+    # gather river dictionary
+    dict_river = stations_by_river(stations)
+    # stations on this river
+    print(output_station_names(dict_river['River Aire'])) # print station names on river aire
+    print(output_station_names(dict_river['River Cam']))
+    print(output_station_names(dict_river['River Thames']))
+    
+def output_station_names(list_stations):
+    list_stations.sort()
+    output = []
+    for station in  list_stations:
+        output.append(station.name)
+    return output
+
 
 if __name__ == "__main__":
     print("*** Task 1A: CUED Part IA Flood Warning System ***")

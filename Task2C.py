30980be51ea569f631508c207ef91350adc921b3
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-02-27 23:59:11 +0000

    Task-2C help help help help help

--- Task2C.py
+++ Task2C.py
@@ -1,6 +1,6 @@
 # region imports
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
 # endregion
 
 """prints the names of the 10 stations at which the current relative level is highest, with the relative level beside each station name"""
@@ -14,6 +14,10 @@ def run():
 
     # Update latest level data for all stations
     update_water_levels(stations)
+
+    station_objects_risk_list = stations_highest_rel_level(stations, 10)
+    for station_object in station_objects_risk_list:
+        print(station_object.name, station_object.relative_water_level())
 
 
 # endregion

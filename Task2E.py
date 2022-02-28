dc41ba3b7c766f5b7017ae433e61dea6f8058973
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-02-28 11:21:12 +0000

    rip

--- Task2E.py
+++ Task2E.py
@@ -1,7 +1,7 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.plot import plot_water_levels
 from floodsystem.flood import stations_highest_rel_level
-import datetime
+from floodsystem.datafetcher import fetch_measure_levels
 
 
 def run():
@@ -12,20 +12,18 @@ def run():
 
     # Plot water levels over past 10 days for 5 stations at which current rel. water level is highest.
 
-    # Create list of past 5 days
-    date_list = []
-
-    for i in range(5):
-        date_item = datetime.date.today() - datetime.timedelta(days=i)
-        date_list.append(date_item)
+    N = 5
 
     # Create list of top N stations
-    selected_stations_list = stations_highest_rel_level(stations, 5)
+    selected_stations_list = stations_highest_rel_level(stations, N)
 
-    # Create list of latest_level data for past 5 days
+    # Create list of level data for past 5 days
+    level_list = []
+    for i in range(N):
+        fetch_measure_levels(measure_id, 10)
 
     # Plot
-    plot_water_levels(selected_stations_list, date_list, levels)
+    plot_water_levels(selected_stations_list, date_list, level_list)
 
 
 if __name__ == "__main__":

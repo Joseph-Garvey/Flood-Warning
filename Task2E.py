273a9dc5287e154f97eaea12cf8de43a05351d2f
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-02-28 11:45:56 +0000

    Fixed fetch inputs

--- Task2E.py
+++ Task2E.py
@@ -2,6 +2,7 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.plot import plot_water_levels
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.datafetcher import fetch_measure_levels
+import datetime
 
 
 def run():
@@ -12,15 +13,14 @@ def run():
 
     # Plot water levels over past 10 days for 5 stations at which current rel. water level is highest.
 
-    N = 5
+    N = 5  # Number of stations
+    dt = 10  # Number of days
 
     # Create list of top N stations
     selected_stations_list = stations_highest_rel_level(stations, N)
 
-    # Create list of level data for past 5 days
-    level_list = []
-    for i in range(N):
-        fetch_measure_levels(measure_id, 10)
+    # Create list of level data for past 10 days
+    date_list, level_list = fetch_measure_levels(measure_id, dt=datetime.timedelta(days=dt))
 
     # Plot
     plot_water_levels(selected_stations_list, date_list, level_list)

a39d22eff83f9b89ccbc1179fb030c01e9b3c341
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-03-01 18:55:22 +0000

    test_predictor

---
+++ test_predictor.py
@@ -0,0 +1,13 @@
+from floodsystem.station import MonitoringStation
+from floodsystem.predictor import predictor, assigning_risk
+
+
+def test_predictor():
+
+    # Create dummy stations
+
+    predictor()
+
+    # assert that attribute type is a float
+
+    # assert that attribute value is as expected

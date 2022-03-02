324cd98bd3a9f86a0b19db8eff2acbdc9098d9b7
Muhamad Irfan Muhamad Syahril <mim38@cam.ac.uk>
2022-03-02 08:53:09 +0000

    Task-2G with 200 stations

--- Task2G.py
+++ Task2G.py
@@ -8,6 +8,7 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.predictor import predictor, assigning_risk
 import matplotlib.dates as dt
 
+
 def run():
     tic = time.perf_counter()
     # Build list of stations
@@ -19,16 +20,18 @@ def run():
     update_water_levels(stations)
     # flood.py retrieve all data [Irfan]
     simduration = 2
-    #stations, dates, levels = stations_highest_rel_level_consistent(stations, 200, simduration)#stations historical for all
-    #stations, dates, levels = stations_highest_rel_level_consistent(stations, len(stations), simduration)#stations historical for all
-    stations, dates, levels = stations_historical(stations, simduration)
+    stations, dates, levels = stations_highest_rel_level_consistent(
+        stations, 200, simduration)  # top 200 rel_level stations
+    # stations, dates, levels = stations_highest_rel_level_consistent(stations, len(stations), simduration) #stations historical for all
+    #stations, dates, levels = stations_historical(stations, simduration)
+
     # compute poly, d for each station
     for i in range(len(stations)):
         poly = polyfit(dates[i], levels[i], 4)
-        #yesterdays date
-        stations[i].der = gradientcalc(poly[0], -simduration/2)
+        # yesterdays date
+        stations[i].der = gradientcalc(poly[0], -simduration / 2)
     # retrieve scores for each station [Irfan]
-    gradient_weight = 1 
+    gradient_weight = 1
     rel_weight = 5
     abs_weight = 15
     stations = predictor(stations, gradient_weight, rel_weight, abs_weight)
@@ -45,6 +48,7 @@ def run():
     # should have done moderate if above typical high
     toc = time.perf_counter()
     print(f"Warned floods in {toc - tic:0.4f} seconds")
+
 
 if __name__ == "__main__":
     print("*** Task 2G: CUED Part IA Flood Warning System ***")
